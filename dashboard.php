3abf2ebe72e40dffa1d476743e7c3f7677b87779
genius-cabana <1513661@senati.pe>
2025-06-15 12:00:21 -0500

    Subiendo los archivos

--- dashboard.php
+++ dashboard.php
@@ -7,8 +7,6 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
-// Incluir sistema de control de cache
-require_once 'includes/cache_control.php';
 
 // Función simple para formatear moneda
 function formatCurrency($amount) {
